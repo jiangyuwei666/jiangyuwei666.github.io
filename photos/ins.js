6049f92ccbd90200d4db7e2b9768728546d44eee
jiangyuwei666 <739843128@qq.com>
2018-09-16 19:54:27 +0800

    Site updated: 2018-09-16 19:54:23

--- photos/ins.js
+++ photos/ins.js
@@ -116,11 +116,11 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/jiangyuwei666/MyBlogBackUp/master/min_photos/' + data.link[i] + '.min.jpg';
+          var minSrc = 'https://raw.githubusercontent.com/jiangyuwei666/MyBlogBackUp/master/min_photos/' + data.link[i];
           var src = 'https://raw.githubusercontent.com/jiangyuwei666/MyBlogBackUp/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '.jpg';
+          src += '';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
