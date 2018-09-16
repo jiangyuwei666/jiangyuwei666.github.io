d6fa61ef05e0a6274aea9846cc51b6bfdde14e07
jiangyuwei666 <739843128@qq.com>
2018-09-16 18:56:44 +0800

    Site updated: 2018-09-16 18:56:42

--- photos/ins.js
+++ photos/ins.js
@@ -120,7 +120,7 @@
           var src = 'https://raw.githubusercontent.com/jiangyuwei666/MyBlogBackUp/blob/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '.jpg';
+          src += '';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
