9de7381d21ace88c9ae8d12c1304ebeed1921796
jiangyuwei666 <739843128@qq.com>
2018-12-05 22:20:05 +0800

    Site updated: 2018-12-05 22:20:04

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://litten.me/ins-min/' + data.link[i] + '.min.jpg';
-          var src = 'http://litten.me/ins/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/jiangyuwei666/MyBlogBackUp/master/min_photos/' + data.link[i] + '.min.jpg';
+          var src = 'https://raw.githubusercontent.com/jiangyuwei666/MyBlogBackUp/master/photos' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
